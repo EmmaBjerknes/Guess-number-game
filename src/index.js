e5158837ba55290481be41244dbb9110ae2194d2
Emma Bjerknes <Bjerknes.Emma@gmail.com>
2023-06-01 08:54:43 +0200

    user input for guess and msg from npm shows

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-console.log("Herp");
+import getAnswerMsg from "test-msg-printer";
+
+window.getAnswerMsg = getAnswerMsg;
 
 const guessWrapper = document.getElementById("guess-wrapper");
-const settingsWrapper = document.getElementById("settings-wrapper");
+const answerWrapper = document.getElementById("answer-wrapper");
+
+const guessInput = document.createElement("input");
+guessInput.type = "number";
+
+const guessBtn = document.createElement("button");
+guessBtn.innerText = "I think it is this one!";
+
+guessWrapper.append(guessInput, guessBtn);
+
+guessBtn.addEventListener("click", () => {
+  answerWrapper.innerHTML = "";
+
+  const userGuess = guessInput.value;
+
+  if (userGuess) {
+    const showAnswer = getAnswerMsg(userGuess);
+    answerWrapper.appendChild(showAnswer);
+
+    setTimeout(() => {
+      answerWrapper.removeChild(showAnswer);
+    }, 2000);
+  }
+});
 
 // mock up for setting difficulty lvl later on
 function findSelectedInput() {
@@ -9,6 +34,7 @@ function findSelectedInput() {
 
   if (selectedInput) {
     console.log(selectedInput.value);
+    return selectedInput.value;
   } else {
     console.log("No input");
   }
